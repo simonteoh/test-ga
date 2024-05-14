637eecbba62214a392dc0371f72707836b0a2276
simon <simonteoh1996@gmail.com>
2024-05-14 09:51:16 +0800

    change send

--- src/main.tsx
+++ src/main.tsx
@@ -5,15 +5,8 @@ import './index.css'
 import ReactGA from "react-ga4";
 
 ReactGA.initialize("G-Z0ZFQL7H73");
-ReactGA.send({ hitType: "pageview", page: "/", title: "Home page" });
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
 
-// Send a custom event
-ReactGA.event({
-  category: "your category",
-  action: "your action",
-  label: "your label", // optional
-  value: 99, // optional, must be a number
-});
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
